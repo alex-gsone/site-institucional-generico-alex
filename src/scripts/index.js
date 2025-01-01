786b0e09aeaf94b1327f5ad98dfa2e40e815ee65
Alex Gomes <alex.gsone@gmail.com>
2025-01-01 17:26:18 -0300

    Update

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,24 +1,2 @@
 /* Desenvolva seu código aqui... */
 
-
-function handleModal() {
-    const button = document.querySelector(".header_button");
-    const modalContainer = document.querySelector(".modal");
-  
-    button.addEventListener("click", () => {
-      modalContainer.showModal();
-  
-      closeModal();
-    });
-  }
-  
-  function closeModal() {
-    const button = document.querySelector("#closeModal");
-    const modalContainer = document.querySelector(".modal");
-  
-    button.addEventListener("click", () => {
-      modalContainer.close();
-    });
-  }
-  
-  handleModal(); 
