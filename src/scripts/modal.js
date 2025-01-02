c5c155e05607f59d269a2f98ddac87993704534b
Alex Gomes <alex.gsone@gmail.com>
2025-01-01 21:00:30 -0300

    Update

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,22 +1,28 @@
-
 function handleModal() {
-    const button = document.querySelector(".header_button");
-    const modalContainer = document.querySelector(".modal");
-  
-    button.addEventListener("click", () => {
-      modalContainer.showModal();
-  
-      closeModal();
-    });
+  const button1 = document.querySelector(".header_button");
+  const button2 = document.querySelector(".faq_button");
+  const modalContainer = document.querySelector(".modal");
+
+  // Função que abre o modal
+  function openModal() {
+    modalContainer.showModal();
   }
-  
-  function closeModal() {
-    const button = document.querySelector("#closeModal");
-    const modalContainer = document.querySelector(".modal");
-  
-    button.addEventListener("click", () => {
-      modalContainer.close();
-    });
-  }
-  
-  handleModal(); 
+
+  // Adiciona evento de clique aos botões para abrir o modal
+  button1.addEventListener("click", openModal);
+  button2.addEventListener("click", openModal);
+
+  closeModal();
+}
+
+function closeModal() {
+  const button = document.querySelector("#closeModal");
+  const modalContainer = document.querySelector(".modal");
+
+  // Função que fecha o modal
+  button.addEventListener("click", () => {
+    modalContainer.close();
+  });
+}
+
+handleModal();
